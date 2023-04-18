b0a45192b03dfbf7dbfdb56620814c7ee1d05123
Maxnovv <novinskii.ms@phystech.edu>
2023-04-18 23:47:58 +0300

    fix

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/web/controllers/LinksControllers.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/web/controllers/LinksControllers.java
@@ -2,6 +2,7 @@ package ru.tinkoff.edu.java.scrapper.web.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,28 +10,35 @@ import ru.tinkoff.edu.java.scrapper.web.dto.controllers.AddLinkRequest;
 import ru.tinkoff.edu.java.scrapper.web.dto.controllers.LinkResponse;
 import ru.tinkoff.edu.java.scrapper.web.dto.controllers.ListLinksResponse;
 import ru.tinkoff.edu.java.scrapper.web.dto.controllers.RemoveLinkRequest;
+import ru.tinkoff.edu.java.scrapper.web.services.LinkService;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/links")
 public class LinksControllers {
+
+    @Autowired
+    LinkService service;
+
     @GetMapping
     @Operation(summary = "Получить все отслеживаемые ссылки")
-    public ResponseEntity<ListLinksResponse> getLinks(@RequestHeader("Tg-Chat-Id") int chatId)
+    public ResponseEntity<?> getLinks(@RequestHeader("Tg-Chat-Id") int chatId)
     {
-        return new ResponseEntity<>(new ListLinksResponse(1,new LinkResponse[]{new LinkResponse(1, "string")}), HttpStatus.OK);
+        return service.listAll(chatId);
     }
 
     @PostMapping
     @Operation(summary = "Добавить отслеживание ссылки")
-    public ResponseEntity<LinkResponse> postLink(@RequestHeader("Tg-Chat-Id") int chatId, @Valid @RequestBody AddLinkRequest request)
+    public ResponseEntity<?> postLink(@RequestHeader("Tg-Chat-Id") int chatId, @Valid @RequestBody AddLinkRequest request)
     {
-        return new ResponseEntity<>(new LinkResponse(1,"link"),HttpStatus.OK);
+        return service.add(chatId, URI.create(request.link()));
     }
 
     @DeleteMapping
     @Operation(summary = "Убрать отслеживание ссылки")
-    public ResponseEntity<LinkResponse> deleteLink(@RequestHeader("Tg-Chat-Id") int chatId,@Valid @RequestBody RemoveLinkRequest request)
+    public ResponseEntity<?> deleteLink(@RequestHeader("Tg-Chat-Id") int chatId,@Valid @RequestBody RemoveLinkRequest request)
     {
-        return new ResponseEntity<>(new LinkResponse(1,"link"),HttpStatus.OK);
+        return service.remove(chatId,URI.create(request.link()));
     }
 }
--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/web/controllers/TgChatControllers.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/web/controllers/TgChatControllers.java
@@ -1,24 +1,28 @@
 package ru.tinkoff.edu.java.scrapper.web.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.tinkoff.edu.java.scrapper.web.services.TgChatService;
 
 @RestController
 @RequestMapping(value = "tg-chat/{id}")
 public class TgChatControllers {
+    @Autowired
+    TgChatService service;
     @PostMapping
     @Operation(summary = "Зарегистрировать чат")
     public ResponseEntity<?> registerChat(@PathVariable int id)
     {
-        return ResponseEntity.ok().build();
+        return service.register(id);
     }
 
     @DeleteMapping
     @Operation(summary = "удалить чат")
     public ResponseEntity<?> deleteChat(@PathVariable int id)
     {
-        return ResponseEntity.ok().build();
+        return service.unregister(id);
     }
 }
 
